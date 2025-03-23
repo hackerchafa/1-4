fd161f322e55b9ab245a6aafdb8116680216842c
hackerchafa <hmgd.14@gmail.com>
2025-03-22 23:44:19 -0600

    Add files via upload

---
+++ Inversa/Lectura/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Inversa
+{
+    class Inversa
+    {
+        public int[] array { get; set; }
+        public int numeros { get; private set; }
+
+        public Inversa(int[] array, int numeros)
+        {
+            this.array = array;
+            this.numeros = numeros;
+        }
+
+        public void inversa()
+        {
+            Console.WriteLine("\nNumeros ingresados: ");
+
+            for (int i = 0; i < numeros; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.WriteLine("\nNumeros en orden inverso: ");
+
+            for (int i = numeros - 1; i >= 0; i--)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+    }
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            int numeros = 5;
+            int[] array = new int[5];
+
+            Console.WriteLine("Ingrese 5 numeros");
+
+            for (int i = 0; i < numeros; i++)
+            {
+                Console.WriteLine("\nIngresa el numero {0}", i + 1);
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Inversa miInversa = new Inversa(array, numeros);
+            miInversa.inversa();
+        }
+    }
+}
---
+++ Ordenamiento/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Ordenamiento
+{
+    class Ordenamiento
+    {
+        public int[] array { get; set; }
+        public int cantidad { get; set; }
+
+        public Ordenamiento(int[] array, int cantidad)
+        {
+            this.array = array;
+            this.cantidad = cantidad;
+            BubbleSort(array);
+        }
+
+        public void BubbleSort(int[] array)
+        {
+            int n = array.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        public void mostrarInfo()
+        {
+            Console.WriteLine($"\nNumeros ordenados: ");
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+    class Program 
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cuantos numeros deseas ordenar?");
+            int cantidad = int.Parse(Console.ReadLine());
+
+            int[] array = new int[cantidad];
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.WriteLine("Ingresa el numero {0}", i + 1);
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Ordenamiento miOrdenamiento = new Ordenamiento(array, cantidad);
+            miOrdenamiento.mostrarInfo();
+        }
+    }
+}
