a9e5d98758f43278caf42946fd283aeaeed44174
hackerchafa <hmgd.14@gmail.com>
2025-03-22 23:43:17 -0600

    Add files via upload

---
+++ Busqueda/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Busqueda
+{
+    class Busqueda
+    {
+        public string[] nombres { get; set; }
+        public string nombreBuscar { get; private set; }
+
+        public Busqueda(string[] nombres, string nombreBuscar)
+        {
+            this.nombres = nombres;
+            this.nombreBuscar = nombreBuscar;
+            Buscar();
+        }
+
+        public void Buscar()
+        {
+            bool encontrado = false;
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                if (nombres[i] == nombreBuscar)
+                {
+                    Console.WriteLine("El nombre {0} se encuentra en la posicion {1}", nombreBuscar, i + 1);
+                    encontrado = true;
+                    break;
+                }
+            }
+
+            if (!encontrado)
+            {
+                Console.WriteLine("El nombre {0} no se encuentra en la lista", nombreBuscar);
+            }
+        }
+
+    }
+
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            string[] nombres = new string [5];
+
+            Console.WriteLine("Escribe 5 nombres");
+
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Console.WriteLine("Nombre {0}", i + 1);
+                nombres[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nIngrese el nombre a buscar");
+            string nombreBuscar = Console.ReadLine();
+
+            Busqueda miBusqueda = new Busqueda(nombres, nombreBuscar);
+        }
+    }
+}
---
+++ Suma/Promedio/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Suma_Promedio
+{
+    class Numeros
+    {
+        public int[] numeros { get; set; }
+        public int suma { get; private set; }
+        public double promedio { get; private set; }
+
+        public Numeros(int[] numeros)
+        {
+            this.numeros = numeros;
+            Sumar_Promedio();
+        }
+
+        public void Sumar_Promedio()
+        {   
+            suma = 0;
+            foreach (int num in numeros)
+            {
+                suma += num;
+            }
+
+            promedio = (double)suma / numeros.Length;
+        }
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"La suma de los numeros es: {suma}");
+            Console.WriteLine($"El promedio de los numeros es: {promedio}");
+        }
+    }
+
+    class Program
+    {
+       static void Main(string[] args)
+       {
+            int[] numeros = new int[10];
+
+            Console.WriteLine("Ingrese 10 numeros enteros");
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine($"Numero {i + 1}: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            Numeros misNumeros = new Numeros(numeros);
+            misNumeros.MostrarInfo();
+       }
+    }
+}
